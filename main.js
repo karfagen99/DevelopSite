edc2aed2f9bfdbbb709bb148f53c1500aafa31d4
karfagen99 <mrtester07@mail.ru>
2022-08-31 19:37:09 +0300

    minor update

--- main.js
+++ main.js
@@ -98,13 +98,13 @@ buttons.forEach(function(but)
     })
 
 })
-function gir() {  
-     nums = document.getElementById('nums_1').innerHTML;  
-     console.log(nums);
-    if(nums == 1) {document.getElementById('gir').className='gir_1';document.getElementById('nums_1').innerHTML='2'}  
-    if(nums == 2) {document.getElementById('gir').className='gir_2';document.getElementById('nums_1').innerHTML='3'}  
-    if(nums == 3) {document.getElementById('gir').className='gir_3';document.getElementById('nums_1').innerHTML='1'}  
-    }  
-    setInterval(function(){gir()}, 500)
+// function gir() {  
+//      nums = document.getElementById('nums_1').innerHTML;  
+//      console.log(nums);
+//     if(nums == 1) {document.getElementById('gir').className='gir_1';document.getElementById('nums_1').innerHTML='2'}  
+//     if(nums == 2) {document.getElementById('gir').className='gir_2';document.getElementById('nums_1').innerHTML='3'}  
+//     if(nums == 3) {document.getElementById('gir').className='gir_3';document.getElementById('nums_1').innerHTML='1'}  
+//     }  
+//     setInterval(function(){gir()}, 500)
 
     
